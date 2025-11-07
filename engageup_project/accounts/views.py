97c8f9fb35326abfd5b0f2d3ffbc2649926c6f14
Fujisaki Yuito <yuitondonton365@gmail.com>
2025-11-07 13:51:29 +0900

    htmlある程度追加

--- engageup_project/accounts/views.py
+++ engageup_project/accounts/views.py
@@ -1,5 +1,12 @@
+from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render, redirect
-from django.contrib.auth import authenticate, login, logout
-from django.contrib import messages
-from django.contrib.auth.decorators import login_required
 
+def signup(request):
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')  # ログインページへリダイレクト
+    else:
+        form = UserCreationForm()
+    return render(request, 'accounts/1102.html', {'form': form})
