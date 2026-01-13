d17e79dabd3ef50a7149e359deea73d873715aa9
satosi <kirito.eugeo@icloud.com>
2026-01-13 15:51:23 +0900

    HTMLテンプレート

--- engageup_project/visitor/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path("", views.visitor_indexindex, name="visitor_index"),  # トップページ用
-]
--- engageup_project/visitor/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-
-
-def visitor_index(request):
-    return render(request,"visitor/visitor_index.html")
