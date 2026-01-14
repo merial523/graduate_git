24857056d13dff38660ccb432e330281ed0ec725
ぼくみつき <mitsuki170604@gmail.com>
2026-01-14 16:06:45 +0900

    aiQuestion

--- engageup_project/ai_question/urls.py
+++ engageup_project/ai_question/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-
+    path(
+        "ai_question/", views.aiQuestion_Index, name="aiQuestionIndex"
+    ),  # トップページ用
 ]
--- engageup_project/ai_question/views.py
+++ engageup_project/ai_question/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+def aiQuestion_Index(request):
+    return render(request, "ai_question/aiQuestion_index.html")
