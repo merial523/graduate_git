5565dbeb3cc0859913bf7f4ca42870d6020ddd4d
Fujisaki Yuito <yuitondonton365@gmail.com>
2026-01-15 14:41:37 +0900

    accounts完了

--- engageup_project/moderator/urls.py
+++ engageup_project/moderator/urls.py
@@ -4,10 +4,6 @@ from . import views
 app_name = "moderator"
 urlpatterns = [
     path("", views.moderator_index, name="moderator_index"),  # トップページ用
-<<<<<<< HEAD
-    path("create-user",views.SequentialUserCreateView.as_view(),name = "moderator_create_user")#ユーザーを作成する
-    
-=======
     path(
         "create-user",
         views.SequentialUserCreateView.as_view(),
@@ -19,5 +15,4 @@ urlpatterns = [
     path(
         "moderator_news/", views.moderator_news, name="moderatorNews"
     ),  # お知らせ投稿ページ
->>>>>>> e4deb16fd02962dae225d993c78425e76b4b83a8
 ]
