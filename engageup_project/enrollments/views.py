4af623b802cab006b08d524a5caf0345ab0fa60c
ぼくみつき <mitsuki170604@gmail.com>
2026-01-15 10:30:16 +0900

    ハンバーガーメニュー変更

--- engageup_project/enrollments/views.py
+++ engageup_project/enrollments/views.py
@@ -6,3 +6,15 @@ def enrollments_index(request):
     return render(
         request, "enrollments/5101.html"
     )  # アプリ内 templates/enrollments/index.html を参照
+
+
+def enrollments_create(request):
+    return render(
+        request, "enrollments/mo_exam_create.html"
+    )  # アプリ内 templates/enrollments/index.html を参照
+
+
+def enrollments_history(request):
+    return render(
+        request, "enrollments/history_exam.html"
+    )  # アプリ内 templates/enrollments/index.html を参照
