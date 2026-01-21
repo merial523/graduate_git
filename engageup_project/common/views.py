a66c29779693b4648ffc53d579375ca60aea0a3a
Fujisaki Yuito <yuitondonton365@gmail.com>
2026-01-21 12:16:11 +0900

    BaseCreateView作成完了

--- engageup_project/common/views.py
+++ engageup_project/common/views.py
@@ -25,3 +25,21 @@ class BaseCreateView(CreateView):
             return reverse_lazy("staff:staff_index")
         elif rank == "vistor":
             return reverse_lazy("vistor:vistor_index")
+        #共通で返す処理
+class BaseCreateView(CreateView):
+    is_continue = None      #続けて保存か判定
+    is_continue_url = ""    #送信先のURL
+    #権限を分ける関数　
+    def get_success_url(self):
+        if self.is_continue:
+            return reverse_lazy(self.is_continue_url)
+        rank = self.request.user.rank
+        if rank == "administer":
+            return reverse_lazy("administer:administer_index")
+        elif rank == "moderator":
+            return reverse_lazy("moderator:moderator_index")
+        elif rank == "staff":
+            return reverse_lazy("staff:staff_index")
+        elif rank == "vistor":
+            return reverse_lazy("vistor:vistor_index")
+        
